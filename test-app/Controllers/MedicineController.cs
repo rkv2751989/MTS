7fa630af01673f8748fe49d9e0df03e04eca119d
rkv2751989 <44620028+rkv2751989@users.noreply.github.com>
2020-07-07 10:58:06 +0530

    Add project files.

---
+++ test-app/Controllers/MedicineController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using test_app.Models;
+
+namespace test_app.Controllers
+{
+    public class MedicineController : ApiController
+    {
+       
+
+       static IList<Medicine> medicines = new List<Medicine>()
+        {
+            new Medicine()
+                {
+                    FullName = "PeracitaMol", Brand = "Cipla", Price = 100, Quantity = 10, Notes=""
+                },
+        };
+
+        [HttpGet]
+        public IList<Medicine> GetAllMedicines()
+        {
+            return medicines;
+        }
+        [HttpGet]
+        public Medicine GetMedicinesDetails(string name)
+        {
+          
+            var medicine = medicines.FirstOrDefault(e => e.FullName == name);
+            if (medicine == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return medicine;
+        }
+
+        [HttpPost]
+        public void post([FromBody] Medicine medicine)
+        {
+
+            medicines.Add(medicine);
+        }
+
+    }
+}
---
+++ test-app/Models/Medicine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_app.Models
+{
+    public class Medicine
+    {
+        public int id { get; set; }
+        public string  FullName
+        {
+            get;
+            set;
+        }
+        public string Brand
+        {
+            get;
+            set;
+        }
+        public decimal Price
+        {
+            get;
+            set;
+        }
+        public int Quantity { get; set; }
+
+        public DateTime Expiry_Date { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
